48a2535911aefcc607b0c005508ab16c82f0bf2a
Jorge Argüelles <180930743+JorgeAdx@users.noreply.github.com>
2025-09-19 17:46:46 -0600

    Agregar funcionalidades faltantes y corregir error de fecha futura

--- Calculadora_Edad2.js
+++ Calculadora_Edad2.js
@@ -16,8 +16,11 @@ if (partes.length !== 3) {                                                      
 
     if (isNaN(diaNac) || isNaN(mesNac) || isNaN(annioNac)) {                    // Verificar que las unidades sean números
         console.log("La fecha contiene valores no numéricos."); 
-    } else if (annioNac < 1900 || annioNac > fechaHoy.annioHoy) {               // Verificar casos en los que el año sea muy antiguo o futuro
-        console.log("El año debe estar entre 1900 y 2025.");
+    } else if (annioNac < 1900 ||                                               // Verificar casos en los que el año sea muy antiguo o una fecha futura
+    annioNac > annioHoy ||
+    (annioNac === annioHoy && mesNac > mesHoy) ||
+    (annioNac === annioHoy && mesNac === mesHoy && diaNac > diaHoy)) {          
+        console.log("La fecha debe estar entre 1900 y el día de hoy.");
     } else if (mesNac < 1 || mesNac > 12 || diaNac < 1 || diaNac > 31) {        // Verificar casos para dias y meses fuera del rango 1-31 y 1-12 respectivamente
         console.log("La fecha ingresada no es válida.");
     } else {
@@ -28,7 +31,12 @@ if (partes.length !== 3) {                                                      
             fechaNacimiento.getDate() !== diaNac
         ) {
             console.log("La fecha ingresada no existe.");        
-        } else {                                                                // Si la fecha es correcta, entonces se procede con la operación
+        } else {                                                                // Si la fecha es correcta, entonces se procede con la operación                                                                
+            if ((annioNac % 4 === 0 && annioNac % 100 !== 0) ||                 // Si el año de nacimiento es bisiesto se menciona en el programa
+            (annioNac % 400 === 0)){
+                console.log(`Tu año de nacimiento ${annioNac} es bisiesto`);
+
+            }             
             let edad = annioHoy - annioNac;                                     // Se hace el cálculo restando el año actual con el año de nacimiento
             if (                                                                // Se verifica si ha cumplido años en el año actual o no
                 mesNac > mesHoy ||
@@ -36,8 +44,12 @@ if (partes.length !== 3) {                                                      
             ) {
                 edad--;
             }
-            console.log(`Tienes ${edad} años.`);                                // Muestra un mensaje con la edad actual del usuario
-            
+            if (diaNac === diaHoy && mesNac === mesHoy){                        // Si el coincide la fecha de nacimiento con la actual, se felicita al usuario
+                console.log(`¡Feliz cumpleaños! Ya tienes ${edad} años.`);
+            }
+            else {
+                console.log(`Tienes ${edad} años.`);                            // Muestra un mensaje con la edad actual del usuario
+            }           
         }
     }
 }
